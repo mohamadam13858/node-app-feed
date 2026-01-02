67d58e5a2994f05bfbdddde92a615cf4cea19de6
mohammad <mh177148@gmail.com>
2026-01-02 15:06:36 +0330

    model,errorHandle,staticImage

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express')
+const path = require('path')
 const app = express()
+require('dotenv').config()
 const bodyParser = require('body-parser')
 const feedRotes = require('./routes/feed')
 const mongoose = require('mongoose')
 
 app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin' , '*')
-    res.setHeader('Access-Control-Allow-Methods' , 'GET , POST , PUTH , PATCH , DELETE')
-    res.setHeader('Access-control-Allow-Headers'  , 'Content-type, Authorization')
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET , POST , PUTH , PATCH , DELETE')
+    res.setHeader('Access-control-Allow-Headers', 'Content-type, Authorization')
     next()
 })
 
 app.use('/feed', feedRotes)
+app.use((error, req , res , next) => {
+    console.log(error) 
+    const status = error.statusCode || 500
+    const message = error.message 
+    res.status(status).json({message: message})
+
+})
 mongoose.connect(process.env.MONGODB_URI).then(result => {
     app.listen(8080)
-}).catch(err => console.log(err))
+}).catch(err => console.log(err))   
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,4 +1,5 @@
-const {validationResult} = require('express-validator')
+const { validationResult } = require('express-validator')
+const Post = require('../models/post')
 
 
 exports.getPosts = (req, res, next) => {
@@ -16,19 +17,28 @@ exports.getPosts = (req, res, next) => {
 
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-         return res.status(422).json({message: 'Validation failed' , errors: errors.array()}) 
+    if (!errors.isEmpty()) { 
+        const error = new Error('Validation failed')
+        error.statusCode = 422 
+        throw error
     }
     const title = req.body.title
     const content = req.body.content
-    res.status(201).json({
-        message: 'Post created successfully!',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: { name: 'mohamad' },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        imageUrl: 'images/mm.png',
+        content: content,
+        creator: { name: 'mohamad' },
+    })
+    post.save().then(result => {
+        res.status(201).json({
+            message: 'Post created successfully!',
+            post: result
+        })
+    }).catch(err => {
+        if (!err.statusCode) {
+            err.statusCode = 500
         }
+        next(err)
     })
 }
