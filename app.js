6c90969d0f329fde2cb0228e413d7344f5d99dcb
mohammad <mh177148@gmail.com>
2026-01-05 13:45:04 +0330

    socket

--- app.js
+++ app.js
@@ -48,9 +48,12 @@ app.use((error, req, res, next) => {
     const status = error.statusCode || 500
     const message = error.message
     const data = error.data
-    res.status(status).json({ message: message , data:data })
-
+    res.status(status).json({ message: message, data: data })
 })
 mongoose.connect(process.env.MONGODB_URI).then(result => {
-    app.listen(8080)
+    const server = app.listen(8080)
+    const io = require('./socket').init(server)
+    io.on('connection' , socket => {
+        console.log('connect')
+    })
 }).catch(err => console.log(err))   
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
 const { validationResult } = require('express-validator')
+const io = require('../socket')
 const Post = require('../models/post')
 const User = require('../models/user')
 
@@ -48,6 +49,7 @@ exports.createPost = async (req, res, next) => {
         const user = await User.findById(req.userId)
         user.posts.push(post)
         await user.save()
+        io.getIo().emit('posts', { action: 'create', post: post })
         res.status(201).json({
             message: 'Post created successfully!',
             post: post,
