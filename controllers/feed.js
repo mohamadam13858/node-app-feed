88a203641b95cc41c4b3c16d50649dddffffc0ec
mohammad <mh177148@gmail.com>
2026-01-07 22:29:39 +0330

    updateAndcreate-socket

--- controllers/feed.js
+++ controllers/feed.js
@@ -12,7 +12,7 @@ exports.getPosts = async (req, res, next) => {
     const perPage = 2
     try {
         const totalItems = await Post.find().countDocuments()
-        const posts = await Post.find().skip((currentPage - 1) * perPage).limit(perPage)
+        const posts = await Post.find().populate('creator').sort({ createdAt: -1 }).skip((currentPage - 1) * perPage).limit(perPage)
         res.status(200).json({ message: 'Fetched post sucssesfuly', posts: posts, totalItems: totalItems })
     } catch (err) {
         if (!err.statusCode) {
@@ -49,7 +49,7 @@ exports.createPost = async (req, res, next) => {
         const user = await User.findById(req.userId)
         user.posts.push(post)
         await user.save()
-        io.getIo().emit('posts', { action: 'create', post: post })
+        io.getIo().emit('posts', { action: 'create', post: { ...post._doc, creator: { _id: req.userId, name: user.name } } })
         res.status(201).json({
             message: 'Post created successfully!',
             post: post,
@@ -86,7 +86,7 @@ exports.getPost = (req, res, next) => {
 
 
 
-exports.updatePost = (req, res, next) => {
+exports.updatePost = async (req, res, next) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
         const error = new Error('Validation failed')
@@ -105,14 +105,14 @@ exports.updatePost = (req, res, next) => {
         error.statusCode = 422
         throw error
     }
-
-    Post.findById(postId).then(post => {
+    try {
+        const post = await Post.findById(postId).populate('creator')
         if (!post) {
             const error = new Error('could not found Post')
             error.statusCode = 404
             throw error
         }
-        if (post.creator.toString() !== req.userId) {
+        if (post.creator._id.toString() !== req.userId) {
             console.log(post.creator.toString(), req.userId)
             const error = new Error('Not authorezed!')
             error.statusCode = 403
@@ -124,15 +124,15 @@ exports.updatePost = (req, res, next) => {
         post.title = title
         post.imageUrl = imageUrl
         post.content = content
-        return post.save()
-    }).then(result => {
+        const result = post.save()
+        io.getIo().emit('posts', { action: 'update', post: result })
         res.status(200).json({ message: 'updated post ', post: result })
-    }).catch(err => {
+    } catch (err) {
         if (!err.statusCode) {
             err.statusCode = 500
         }
         next(err)
-    })
+    }
 }
 
 
